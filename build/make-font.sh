bbbfe140a61e36fa062a5432dbace75c82846fe9
Sergey Kurdin <kurdin@gmail.com>
2024-04-11 19:50:36 -0400

    update to packages dependencies

---
+++ build/make-font.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# build dependencies: fonttools and curl. in my WSL, fonttools was a single `apt
+# install fonttools` away.
+
+# download latest Twemoji font
+# TTF=$(mktemp -t XXXXXXXXXX.ttf)
+# curl --location ../https://github.com/mozilla/twemoji-colr/releases/latest/download/TwemojiMozilla.ttf --output $TTF
+
+# strip all characters except country flags, and save to woff2.
+#
+# "FFTM" is a metadata table in created by FontForge which TwemojiMozilla
+# somehow includes. pyftsubset can't subset it and warns, so we explicitly drop
+# it to suppress the warning. we don't need it.
+#
+# `unicodes` is used to only select glyphs that can be built with the selected
+# unicode ranges. we need these ranges:
+#   - U+1F1E6-1F1FF the range of regional indicator symbols, combinations of
+#     which make regular flag emojis
+#   - Ingredients for England, Scotland and Wales flags:
+#     - U+1F3F4 is "Waving black flag"
+#     - U+E0061-E007A are Latin Small Letter "Tags"
+#     - U+E007F the "Cancel tag"
+pyftsubset ./Twemoji.Mozilla.ttf \
+  --no-subset-tables+=FFTM \
+  --unicodes=U+1F1FA,U+1F1F8,U+1F1F7,U+1F1FA,U+1F1FA,U+1F1E6U+E0077,U+E007F,U+1F1E9,U+1F1EA,U+1F1EA,U+1F1F8,U+1F1EB,U+1F1F7 \
+  --output-file=./TwemojiCountryFlags.woff2 \
+  --flavor=woff2
+
+echo "Done! Created TwemojiCountryFlags.woff2 in current folder"
+# rm $TTF
